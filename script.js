a208df7c2e4be82d2787415913096d6ede451756
0516233001-star <0516233001@s.tusd1.org>
2026-05-04 21:51:54 +0000

    answers

--- script.js
+++ script.js
@@ -41,7 +41,88 @@ let questions = [
     {
         question: "Who won the 2024 Nba Finals",
         correctAnswer: "celtics"
-    }
+    },
+    {
+        question: "How many Nfl teames are there?",
+        correctAnswer: "32"
+    },
+    {
+        question: "How many Nba teams are there?",
+        correctAnswer: "30"
+    },
+    {
+        question: "How many championships do the Celtics have?",
+        correctAnswer: "18"
+    },
+    {
+        question: "How many rings does Stephen Curry have?",
+        correctAnswer: "4"
+    },
+    {
+        question: "How many rings does Lebron James have?",
+        correctAnswer: "4"
+    },
+    {
+        question: "What sport does Cristiano Ronaldo play?",
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "Kobe Bryant played for what team",
+        correctAnswer: " los angeles lakers"
+    },
+    {
+        question: "Stephen Curry plays for what team?",
+        correctAnswer: "golden state warriors"
+    },
+    {
+        question: "Who is the only unanimous MVP in Nba history? ",
+        correctAnswer: "Stephen Curry"
+    },
+    {
+        question: "What was the second team Wayne Gretzky played for?",
+        correctAnswer: "los angles kings"
+    },
+    {
+        question: "What team does Paul Skenes play for?",
+        correctAnswer: "Pittsburgh Pirates"
+    },
+    {
+        question: "Who is the only person in Nba history to score 100 points?",
+        correctAnswer: "wilt chamberlain"
+    },
+    {
+        question: "How many teams did Wayne Gretzky play for?",
+        correctAnswer: "4"
+    },
+    {
+        question: "What player has the most rings in the Nba?",
+        correctAnswer: "bill russell"
+    },
+     {
+        question: "How many rings does Bill Russell have?",
+        correctAnswer: "11"
+    },
+     {
+        question: "What Nba player is nicknamed the Chef?",
+        correctAnswer: "Stephen Curry"
+    },
+
+    {
+        question: "What is Lebron James nickname?",
+        correctAnswer: "King James"
+    },
+    {
+        question: "What is Kobe Bryant's nickname?",
+        correctAnswer: "the black mamba"
+    },
+    {
+        question: "What center in Nba likes Horses?",
+        correctAnswer: "Nikola Jokic"
+    }, 
+    {
+        question: "What Nba player is know for there shoes brand Jordan?",
+        correctAnswer: "Micheal Jordan"
+    },
 ];
 
 let currentQuestionIndex = 0;
@@ -99,3 +180,9 @@ function startGame() {
     let gameScreen = document.getElementById("game-screen");
     gameScreen.style.display = "block";
 }
+function startGame() {
+    alert("Game started!");
+}
+
+let startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startGame);
